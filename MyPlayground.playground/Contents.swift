060e9beed0b0369c32e7e40b93e74147a6b0e608
Eric Goodwin <eric.goodwin@mlb.com>
2018-10-16 11:02:25 -0400

    added riders and logic

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,12 +6,33 @@ var riders: [Int: String] = [07: "James Stewart", 27: "Malcolm Stewart", 03: "El
 riders[23] = "Aaron Plessinger"
 riders[2] = "Cooper Webb"
 riders[15] = "Dean Wilson"
-
+riders[21] = "Jason Anderson"
+riders[28] = "Martin Davalos"
+riders[51] = "Justin Barcia"
+riders[451] = "Rashidi Kerrison"
+riders[199] = "Travis Pastrana"
+riders[303] = "Eric Goodwin"
+riders[278] = "Ocey Parker Jr"
+riders[78] = "Ocey Parker"
+riders[428] = "Rasheed Shakur"
+riders[751] = "Josh Alfano"
+riders[15] = "Dean Wilson"
+riders[15] = "Dean Wilson"
+riders[15] = "Dean Wilson"
 
 
 print("\n Tonight there will be \(riders.count) Riders lining up at the gate ")
 for (raceNum, riderName )in riders{
- print ("\n \(raceNum) of \(riderName)")
+ print ("\n The \(raceNum) of \(riderName)")
 }
 
+let gate = riders.count
+let need = 22 - riders.count
+
+if gate < 20{
+    print(" \n We need \(need) more riders to sign up")
+    
+}else{
+    print("\n We have enough riders\n Lets Go Racing!!")
+}
 
