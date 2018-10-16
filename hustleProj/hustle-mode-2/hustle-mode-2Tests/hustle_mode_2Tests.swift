d72c83d954830620399b5c402541f3589ba579be
Eric Goodwin <eric.goodwin@mlb.com>
2018-10-16 12:56:27 -0400

    hustle

---
+++ hustleProj/hustle-mode-2/hustle-mode-2Tests/hustle_mode_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  hustle_mode_2Tests.swift
+//  hustle-mode-2Tests
+//
+//  Created by Goodwin, Eric on 10/16/18.
+//  Copyright © 2018 Goodwin, Eric. All rights reserved.
+//
+
+import XCTest
+@testable import hustle_mode_2
+
+class hustle_mode_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
