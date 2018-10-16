1a74fa516102c8967eca5abf9dfac6ff6eeb7324
Eric Goodwin <eric.goodwin@mlb.com>
2018-10-16 09:51:32 -0400

    Added Content

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,9 +3,15 @@ import UIKit
 var str = "Hello, playground"
 
 var riders: [Int: String] = [07: "James Stewart", 27: "Malcolm Stewart", 03: "Eli Tomac", 05: "Ryan dungey", 04: "Ricky Carmichael", 22: " Chad Reed"]
+riders[23] = "Aaron Plessinger"
+riders[2] = "Cooper Webb"
+riders[15] = "Dean Wilson"
+
+
+
 print("\n Tonight there will be \(riders.count) Riders lining up at the gate ")
 for (raceNum, riderName )in riders{
  print ("\n \(raceNum) of \(riderName)")
 }
-// this will be a new playground
+
 
