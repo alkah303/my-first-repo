2584ddb3ecd3a939940a36753c8f66d1dfaabcdb
Eric Goodwin <eric.goodwin@mlb.com>
2018-10-16 08:44:04 -0400

    added airports playground

---
+++ Airports.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var namesOfIntegers = [Int: String]()
+
+namesOfIntegers[3] = "three"
+namesOfIntegers[44] = "forty four"
+
+// to clear out a dictionary >>   namesOfIntegers = [:]
+
+
+
+var airports: [ String: String] = ["CYYZ":"Toronto Pearson","KLAX": "Los Angeles International"]
+airports["OMDB"] = "Dubai International"
+airports["KNSA"] = "John Wayne"
+airports["KTEX"] = "Teluride"
+
+//airports = [:]
+//print("the airport's dictionary has: \(airports.count) items")
+//airports["KNSA"] = nil // <---- remove an item from dictionary
+
+if airports.isEmpty{
+    
+    print("We could not locate the airport")
+}else{
+    print("the airport's dictionary has: \(airports.count) items")
+}
+
+for (airportCode, airportName) in airports{
+    print("\n")
+    print("\(airportCode): \(airportName)")
+    
+}
+    
+
