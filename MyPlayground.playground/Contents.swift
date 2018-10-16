7ff87edeb2179e95945c7d79e24bc196a4517059
alkah303 <egoodwin817@gmail.com>
2018-10-16 09:28:28 -0400

    Update Contents.swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,5 +2,9 @@ import UIKit
 
 var str = "Hello, playground"
 
+var riders: [Int: String] = [7: "James Stewart", 27: "Malcolm Stewart", 3: "Eli Tomac", 5: "Ryan dungey", 4: "Ricky Carmichael", 22: " Chad Reed"]
 
+for (raceNum, riderName )in riders{
+ print ("\(raceNum) of \(riderName))
+}
 // this will be a new playground
